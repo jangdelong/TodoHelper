0c4c41ec45dbd95df13a8100182cb119f9857578
jangdelong <jangdelong@qq.com>
2014-01-21 16:04:38 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.debug = true;
+exports.port = 3000;
+exports.email = 'jangdelong@gmail.com';
+exports.app_name = 'TODO Helper For Windows';
+exports.app_desc = 'Graduation Project For 2014';
+
+/*
+* 本地数据库
+**/
+exports.db = 'mongodb://127.0.0.1:27017/todohelper2';
+
